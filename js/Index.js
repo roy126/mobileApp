64e2ed45291912dabfbb4a35c67a8e17a8f158b1
roy126 <roy12612@gmail.com>
2020-03-14 11:58:03 +0400

    14.03.2020

---
+++ js/Index.js
@@ -0,0 +1,58 @@
+let auth_but = document.querySelector('#auth_but');
+
+$('.auth_form').submit(function(e){
+    e.preventDefault();
+    let login = document.querySelector('#login').value;
+    let password = document.querySelector('#password').value;
+    if (login && password) {
+        var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+        var xhr = new XHR();
+        xhr.open('GET', 'https://bpcard.ru/ajax/app.php?app_password=&app_number=2&textFieldUsername=' + login + '&textFieldPassword=' + password, true);
+        xhr.onload = function() {
+            let result = JSON.parse(this.responseText)[0];
+            console.log(result);
+            if(Number(result.code) === 202 || Number(result.code) === 204){
+                document.querySelector('#log_in_result').style.padding = '9px';
+                document.querySelector('#log_in_result').innerHTML = '<span style="color:red;font-width: 700;">' + result.msg + '<span>';
+                document.querySelector('#password').value = '';
+            }else{
+                let person_id = result.person_id;
+                if(person_id){
+                    window.location.href = 'account.html?person_id=' + person_id;
+                }
+            }
+        }
+        xhr.onerror = function() {
+            console.log('Ошибка ' + this.status);
+        }
+        xhr.send();
+    }
+});
+
+auth_but.onclick = function() {
+    let login = document.querySelector('#login').value;
+    let password = document.querySelector('#password').value;
+    if (login && password) {
+        var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+        var xhr = new XHR();
+        xhr.open('GET', 'https://bpcard.ru/ajax/app.php?app_password=&app_number=2&textFieldUsername=' + login + '&textFieldPassword=' + password, true);
+        xhr.onload = function() {
+            let result = JSON.parse(this.responseText)[0];
+            if(Number(result.code) === 202 || Number(result.code) === 204){
+                document.querySelector('#log_in_result').style.padding = '9px';
+                document.querySelector('#log_in_result').innerHTML = '<span style="color:red;font-width: 700;">' + result.msg + '<span>';
+                document.querySelector('#password').value = '';
+            }else{
+                let person_id = result.person_id;
+                if(person_id){
+                    window.location.href = 'account.html?person_id=' + person_id;
+                }
+            }
+        }
+        xhr.onerror = function() {
+            console.log('Ошибка ' + this.status);
+        }
+        xhr.send();
+    }
+};
+
